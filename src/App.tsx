4238e911ce2c52d6b2dc8014cfafe15de4b63b0a
ayesparshh <sparshgupta1001@gmail.com>
2025-02-04 19:09:17 +0530

    dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import SurveyGrid from "./components/SurveyGrid";
+import About from "./components/About";
 import Footer from "./components/Footer";
 import { pageView } from "./utils/analytics.tsx";
 
 function App() {
 	useEffect(() => {
-		// Track page view when component mounts
 		pageView("Survey List Page");
 	}, []);
 
 	return (
-		<div className="min-h-screen bg-[#424242]">
-			<Header />
-			<main className="bg-[#424242]">
-				<SurveyGrid />
-			</main>
-			<Footer />
-		</div>
+		<BrowserRouter>
+			<div className="min-h-screen bg-[#424242]">
+				<Header />
+				<main className="bg-[#424242]">
+					<Routes>
+						<Route path="/" element={<SurveyGrid />} />
+						<Route path="/about" element={<About />} />
+					</Routes>
+				</main>
+				<Footer />
+			</div>
+		</BrowserRouter>
 	);
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
 	return (
 		<header className="w-full bg-[rgb(103,103,103)] shadow-sm sticky top-0 z-50">
 			<div className="max-w-7xl mx-auto px-4 py-6">
-				<div className="flex flex-col items-center">
-					<h1 className="text-3xl font-bold text-white">All Survey Websites</h1>
-					<p className="text-gray-300 mt-1">No Ad, No Affiliates, No Fluff.</p>
+				<div className="relative flex justify-center">
+					<div className="flex flex-col items-center">
+						<h1 className="text-3xl font-bold text-white">
+							All Survey Websites
+						</h1>
+						<p className="text-gray-300 mt-1">
+							No Ad, No Affiliates, No Fluff.
+						</p>
+					</div>
+					<Link
+						to="/about"
+						className="text-white hover:text-gray-200 font-medium absolute right-0 top-1/2 -translate-y-1/2"
+					>
+						About Us
+					</Link>
 				</div>
 			</div>
 		</header>
